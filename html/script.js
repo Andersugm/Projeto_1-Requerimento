309dfff863115a96a39129a194d18e85d47126fe
Andersugm <andersuemp@gmail.com>
2024-08-22 21:20:27 -0300

    atualizar

---
+++ html/script.js
@@ -0,0 +1,12 @@
+function voltarPagina() {
+    window.location.href = "informativa.html"
+}
+function mudarPagina() {
+    window.location.href = "dados_institucionais.html";
+}
+function voltarPaginaFinish() {
+    window.location.href = "requerimento.html"
+}
+function mudarPaginaRequerimento() {
+    window.location.href = "finish.html";
+}
